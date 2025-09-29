3daa909d77808f6c16b9f7d9980de8fb2793a6cf
Avuluri Sivakalyani <asivakalyani@gamil.com>
2025-09-29 12:18:01 +0100

    Day 1: Added basics files for Python, JavaScript, and SQL

---
+++ javascript/javascript/day1_basics.js
@@ -0,0 +1,5 @@
+// Day 1 - JavaScript Basics
+let name = "Kalyani";
+let age = 30;
+console.log("Hello, my name is " + name + " and I am " + age + " years old.");
+console.log("Sum of 10 + 5 = " + (10 + 5));
---
+++ python/python/day1_basics.py
@@ -0,0 +1,5 @@
+# Day 1 - Python Basics
+name = "Kalyani"
+age = 30
+print(f"Hello, my name is {name} and I am {age} years old.")
+print("Sum of 10 + 5 =", 10 + 5)
